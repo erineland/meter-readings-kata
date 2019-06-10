55b4b507047df4f19a2ad99443915a82d3f85f71
Erin Eland <erin@erineland.com>
2019-06-11 00:56:15 +0100

    Updating notes and tests

--- NOTES.md
+++ NOTES.md
@@ -13,3 +13,6 @@ Notes:
 - First I'm setting a slightly cleaner architecture for the Koa server, by extracting out 'routes' into it's own file and importing this
 - I'm also creating a config file to allow me to more easily setup extra config in the future.
 - Next I'm going to have add a method to the data.ts file to extract out data. I'm going to write a failing test for this first.
+- Created a test for the successful read of data
+- Opted to use async/await because it's clean ES6 syntax
+- In the actual implementation I convert the callback based API of SQLite3 into using native Promises which can be awaited.
--- service/src/data.spec.ts
+++ service/src/data.spec.ts
@@ -1,10 +1,8 @@
 import * as data from './data';
 import * as sqlite3 from 'sqlite3';
-const SQLite = sqlite3.verbose();
 import 'jest';
-// jest.mock('sqlite3', () => { });
+import { read } from 'fs';
 const sampleData = require('../sampleData.json');
-const mockAll = jest.fn();
 
 describe('data', () => {
   it('initialize should import the data from the sampleData file', done => {
@@ -28,10 +26,31 @@ describe('data', () => {
   });
 
   describe('When reading meter readings out of the database', () => {
-    it('reads all of the meter readings out of the database', async () => {
-      const meterReadings : any = await data.getAllMeterReadings();
-      expect(meterReadings.length).toEqual(sampleData.electricity.length);
+    describe('When the read operation is successful', () => {
+      it('reads all of the meter readings out of the database', async () => {
+        const meterReadings: any = await data.getAllMeterReadings();
+        expect(meterReadings.length).toEqual(sampleData.electricity.length);
+      });
     });
-  });
 
+    // describe('When the read operation is unsuccessful', () => {
+    //   beforeEach(() => {
+    //     // sqlite3('sqlite3', () => {
+    //     //   Database: () => {
+    //     //     all: jest.fn((query, cb) => cb(new Error('all error'), null));
+    //     //     serialize: cb => cb();
+    //     //   }
+    //     // });
+    //   });
+
+    //   it('Should reject the promise with an error', async () => {
+    //     try {
+    //       const meterReadings: any = await data.getAllMeterReadings();
+    //     } catch (readError) {
+    //       expect(readError.message)
+    //         .toBe('An error poo when attempting to read from the database: all error');
+    //     }
+    //   })
+    // });
+  });
 });
