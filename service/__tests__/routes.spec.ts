01b72ec30da37f38bcdbf002d8418853bc2700be
Erin Eland <erin@erineland.com>
2019-06-11 17:40:41 +0100

    Setting up API endpoint tests.

--- service/__tests__/routes.spec.ts
+++ service/__tests__/routes.spec.ts
@@ -1,29 +1,30 @@
-import * as Koa from 'koa';
-import server from '../src/index';
 import 'jest';
-import request from 'supertest';
-import * as nock from 'nock';
+import * as request from 'supertest';
+// import * as nock from 'nock';
+import server from '../src/index';
+import { config } from '../src/config';
 
-// Test the routes in this file.
-const serviceUrl = 'http://localhost:3000';
+const sampleData = require('../sampleData.json');
+const serviceUrl = 'http://localhost:4000';
 
 describe('routes', () => {
 
     // afterEach(nock.cleanAll);
 
     it('should retrieve a list of meter readings from the database', async () => {
-        const instance = server();
+        const instance = server().listen(config.port);
 
         // Use nock to fake the server response
-        nock(serviceUrl)
-            .get('/readings')
-            .reply(200, [{
-                message: 'from nock'
-            }]);
+        // nock(serviceUrl)
+        //     .get('/readings')
+        //     .reply(200, [{
+        //         message: 'from nock'
+        //     }]);
 
         // Make a GET request to the /readings endpoint.
         // Use supertest to send a request to the server
-
+        const response = await request(instance).get('/readings');
+        expect(response.body).toBe(sampleData);
         // Assert against the response.
 
     });
